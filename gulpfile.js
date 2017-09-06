a4a1f7976a0ffc36c99b2eedbfb844983f079e41
oumho <oumho.p@gmail.com>
2017-09-06 15:21:31 +0700

    add project

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require("gulp-babel");
+const eslint = require("gulp-eslint");
+const livereload = require("gulp-livereload");
+
+gulp.task('views', function buildHTML() {
+  gulp.src('src/pug/**/*.pug')
+	  	.pipe(pug({
+            pretty: true
+        }))
+	  	.pipe(gulp.dest("html"))
+		.pipe(livereload());
+});
+
+gulp.task('sass', function () {
+    gulp.src('src/scss/*.scss')
+    	.pipe(sourcemaps.init())
+	    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	    .pipe(sourcemaps.write())
+	    .pipe(gulp.dest('assets/style'))
+	    .pipe(livereload());
+});
+
+gulp.task('babel', function() {
+	gulp.src("src/js/*.js")
+	    .pipe(babel())
+	    .pipe(gulp.dest("assets/script"))
+	    .pipe(livereload());
+});
+
+gulp.task('eslint', function() {
+	gulp.src(["src/js/*.js","!node_modules/**"])
+	    .pipe(eslint())
+	    .pipe(eslint.format())
+	    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+	livereload.listen();
+	gulp.watch('src/pug/**/*.pug',['views']);
+	gulp.watch('src/scss/*.scss',['sass']);
+	gulp.watch('src/js/*.js',['babel']);
+});
+
+gulp.task('default', ['watch','views','sass','eslint','babel']);
